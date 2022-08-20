ad93205540f43c6c5b8200d8ce36c63d2fc5f10e
joaopedroaquino <aqerhack@gmail.com>
2022-08-20 11:05:36 -0300

    estilizando o background

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,36 +14,66 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              'Pode entrar',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.w700,
+      //backgroundColor: Colors.deepPurple,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 119, 62, 151),
+              Color.fromARGB(255, 179, 114, 223),
+            ],
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.decal,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'Pode entrar',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              '0',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 100,
-                fontWeight: FontWeight.w300,
+            const Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                '0',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 100,
+                  fontWeight: FontWeight.w300,
+                ),
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    style: TextButton.styleFrom(
+                      primary: Colors.purple,
+                      backgroundColor: Colors.white,
+                      fixedSize: const Size(100, 100),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                    onPressed: increment,
+                    child: const Text(
+                      'Entrou',
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    )),
+                const SizedBox(
+                  width: 30,
+                ),
+                TextButton(
                   style: TextButton.styleFrom(
                     primary: Colors.purple,
                     backgroundColor: Colors.white,
@@ -52,32 +82,16 @@ class HomePage extends StatelessWidget {
                       borderRadius: BorderRadius.circular(24),
                     ),
                   ),
-                  onPressed: increment,
+                  onPressed: decrement,
                   child: const Text(
-                    'Entrou',
+                    'Saiu',
                     style: TextStyle(color: Colors.black, fontSize: 16),
-                  )),
-              const SizedBox(
-                width: 30,
-              ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  primary: Colors.purple,
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100, 100),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
                   ),
                 ),
-                onPressed: decrement,
-                child: const Text(
-                  'Saiu',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                ),
-              ),
-            ],
-          )
-        ],
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
